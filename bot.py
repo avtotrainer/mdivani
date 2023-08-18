8c344d5442240175ac194ccc323277b77f72abaf
admindinge <admin@din.ge>
2023-08-18 21:46:30 +0400

    ახალი მოფიქრება

--- bot.py
+++ bot.py
@@ -1,11 +1,4 @@
-import telebot
+# რაღაც ახალი მოვიფიქრე
 
-bot=telebot.TeleBot('6450641365:AAEJyLYrQJkq_HUgmftauH8uOwVpx7krHuo')
+import discord
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'ვაა, ბარო')
-
-bot.polling(non_stop=True)
-
-# დავამატე კომენტარი
